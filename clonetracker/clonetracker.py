b583a65b5ecd953b725a4ec2d8bc8ecce16a4927
Kozejin <drunkenkoz@gmail.com>
2023-05-17 03:01:28 -0400

    [CloneTracker] Updates

--- clonetracker/clonetracker.py
+++ clonetracker/clonetracker.py
@@ -9,46 +9,76 @@ class CloneTracker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def clonetracker(self, ctx, region: str):
-        valid_regions = ["americas", "europe", "asia"]
+    @commands.hybrid_command()
+    @commands.bot_has_permissions(embed_links=True)
+    async def clonetracker(self, ctx, region: str = "all", ladder: str = "all", hardcore: str = "all"):
+        valid_regions = ["americas", "europe", "asia", "all"]
+        valid_ladder = ["ladder", "non-ladder", "all"]
+        valid_hardcore = ["hardcore", "softcore", "all"]
         
         if region.lower() not in valid_regions:
-            await ctx.send("Invalid region specified. Valid regions are: Americas, Europe, Asia.")
+            await ctx.send("Invalid region specified. Valid regions are: Americas, Europe, Asia, All.")
             return
         
-        region_code = await get_region_code(region.lower())
+        if ladder.lower() not in valid_ladder:
+            await ctx.send("Invalid ladder type specified. Valid types are: Ladder, Non-Ladder, All.")
+            return
         
-        url = f"https://diablo2.io/dclone_api.php?region={region_code}&ladder=1"
-
-        async with aiohttp.ClientSession() as session:
-            try:
-                async with session.get(url) as response:
-                    if response.status != 200:
-                        await ctx.send("An error occurred while fetching Uber Diablo information.")
-                        return
-
-                    data = await response.text()
-                    data_json = json.loads(data)
+        if hardcore.lower() not in valid_hardcore:
+            await ctx.send("Invalid hardcore type specified. Valid types are: Hardcore, Softcore, All.")
+            return
+        
+        if region.lower() == "all":
+            regions = ["americas", "europe", "asia"]
+        else:
+            regions = [region.lower()]
+        
+        if ladder.lower() == "all":
+            ladders = ["ladder", "non-ladder"]
+        else:
+            ladders = [ladder.lower()]
+        
+        if hardcore.lower() == "all":
+            hardcore_types = ["hardcore", "softcore"]
+        else:
+            hardcore_types = [hardcore.lower()]
+        
+        embed = discord.Embed(title="Uber Diablo Information", description="Uber Diablo is a boss in Diablo 2.", color=discord.Color.blue())
+        embed.set_footer(text="Data provided by Diablo2.io")
+        
+        for region in regions:
+            region_code = await get_region_code(region)
+            region_name = await get_region_name(region_code)
+            
+            for ladder in ladders:
+                ladder_code = await get_ladder_code(ladder)
+                ladder_type = await get_ladder_type(ladder_code)
+                
+                for hardcore_type in hardcore_types:
+                    hardcore_code = await get_hardcore_code(hardcore_type)
+                    hardcore_name = await get_hardcore_type(hardcore_code)
                     
-                    if not data_json:
-                        await ctx.send(f"No information available for Uber Diablo in the ladder mode for the {region.capitalize()} region.")
-                        return
+                    url = f"https://diablo2.io/dclone_api.php?region={region_code}&ladder={ladder_code}&hc={hardcore_code}"
                     
-                    progress = data_json[0]['progress']
-                    ladder = data_json[0]['ladder']
-                    region_name = await get_region_name(region_code)
-                    ladder_type = await get_ladder_type(ladder)
-                    
-                    embed = discord.Embed(title="Uber Diablo Information", description="Uber Diablo is a boss in Diablo 2.", color=discord.Color.blue())
-                    embed.add_field(name="Progress", value=f"{progress}/6")
-                    embed.add_field(name="Ladder", value=ladder_type)
-                    embed.add_field(name="Region", value=region_name)
-                    embed.set_footer(text="Data provided by Diablo2.io")
-                    
-                    await ctx.send(embed=embed)
-            except aiohttp.ClientError as e:
-                await ctx.send(f"An error occurred while fetching Uber Diablo information: {str(e)}")
+                    async with aiohttp.ClientSession() as session:
+                        try:
+                            async with session.get(url) as response:
+                                if response.status != 200:
+                                    await ctx.send("An error occurred while fetching Uber Diablo information.")
+                                    return
+    
+                                data = await response.text()
+                                data_json = json.loads(data)
+                                
+                                if not data_json:
+                                    embed.add_field(name=f"{region_name} - {ladder_type} - {hardcore_name}", value="No information available.")
+                                else:
+                                    progress = data_json[0]['progress']
+                                    embed.add_field(name=f"{region_name} - {ladder_type} - {hardcore_name}", value=f"Progress: {progress}/6", inline=False)
+                        except aiohttp.ClientError as e:
+                            await ctx.send(f"An error occurred while fetching Uber Diablo information: {str(e)}")
+        
+        await ctx.send(embed=embed)
 
 async def get_region_code(region):
     if region == "americas":
@@ -60,20 +90,44 @@ async def get_region_code(region):
     else:
         return "0"
 
-async def get_region_name(region):
-    if region == "1":
+async def get_region_name(region_code):
+    if region_code == "1":
         return "Americas"
-    elif region == "2":
+    elif region_code == "2":
         return "Europe"
-    elif region == "3":
+    elif region_code == "3":
         return "Asia"
     else:
         return "Unknown"
 
-async def get_ladder_type(ladder):
-    if ladder == "1":
+async def get_ladder_code(ladder):
+    if ladder == "ladder":
+        return "1"
+    elif ladder == "non-ladder":
+        return "2"
+    else:
+        return "0"
+
+async def get_ladder_type(ladder_code):
+    if ladder_code == "1":
         return "Ladder"
-    elif ladder == "2":
+    elif ladder_code == "2":
         return "Non-Ladder"
+    else:
+        return "Unknown"
+
+async def get_hardcore_code(hardcore):
+    if hardcore == "hardcore":
+        return "1"
+    elif hardcore == "softcore":
+        return "2"
+    else:
+        return "0"
+
+async def get_hardcore_type(hardcore_code):
+    if hardcore_code == "1":
+        return "Hardcore"
+    elif hardcore_code == "2":
+        return "Softcore"
     else:
         return "Unknown"
