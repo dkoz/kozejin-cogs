da7fb70208df607b925e1d986b4c6a86189ff90e
Kozejin <drunkenkoz@gmail.com>
2023-05-17 02:29:58 -0400

    [CloneTracker] More specific error handling

--- clonetracker/clonetracker.py
+++ clonetracker/clonetracker.py
@@ -10,14 +10,22 @@ class CloneTracker(commands.Cog):
 
     #Not sure if this will work.
     @commands.command()
-    async def clonetracker(self, ctx):
+    async def diablo(self, ctx):
         url = "https://diablo2.io/dclone_api.php"
         
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                data = await response.json()
+            try:
+                async with session.get(url) as response:
+                    if response.content_type != "application/json":
+                        await ctx.send("An error occurred while fetching Uber Diablo information. Invalid response format.")
+                        return
+
+                    data = await response.json()
+            except aiohttp.ClientError as e:
+                await ctx.send(f"An error occurred while fetching Uber Diablo information: {str(e)}")
+                return
 
         if data["error"]:
-            await ctx.send("An error occurred while fetching Uber Diablo information.")
+            await ctx.send(f"An error occurred while fetching Uber Diablo information: {data['error']}")
         else:
             await ctx.send(f"Uber Diablo is currently {data['clone_status']} on {data['clone_difficulty']} difficulty.")
