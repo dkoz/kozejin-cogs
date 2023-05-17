9de51206d3a6ae539d169adc79e574fbccc83522
Kozejin <drunkenkoz@gmail.com>
2023-05-17 02:34:31 -0400

    [CloneTracker] Changing how I pull from the API

--- clonetracker/clonetracker.py
+++ clonetracker/clonetracker.py
@@ -1,6 +1,7 @@
 import discord
 from redbot.core import commands
 import aiohttp
+import json
 
 class CloneTracker(commands.Cog):
     """CloneTracker for Diablo 2: Resurrected"""
@@ -8,24 +9,25 @@ class CloneTracker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    #Not sure if this will work.
     @commands.command()
-    async def diablo(self, ctx):
-        url = "https://diablo2.io/dclone_api.php"
-        
+    async def clonetracker(self, ctx):
+        url = "https://diablo2.io/dclone_api.php?region=1&ladder=1"
+
         async with aiohttp.ClientSession() as session:
             try:
                 async with session.get(url) as response:
-                    if response.content_type != "application/json":
-                        await ctx.send("An error occurred while fetching Uber Diablo information. Invalid response format.")
+                    if response.status != 200:
+                        await ctx.send("An error occurred while fetching Uber Diablo information.")
                         return
 
-                    data = await response.json()
+                    data = await response.text()
+                    data_json = json.loads(data)
+                    
+                    if not data_json:
+                        await ctx.send("No information available for Uber Diablo in the ladder mode for the Americas region.")
+                        return
+                    
+                    progress = data_json[0]['progress']
+                    await ctx.send(f"Uber Diablo is currently {progress}/6 on the ladder in the Americas region.")
             except aiohttp.ClientError as e:
                 await ctx.send(f"An error occurred while fetching Uber Diablo information: {str(e)}")
-                return
-
-        if data["error"]:
-            await ctx.send(f"An error occurred while fetching Uber Diablo information: {data['error']}")
-        else:
-            await ctx.send(f"Uber Diablo is currently {data['clone_status']} on {data['clone_difficulty']} difficulty.")
