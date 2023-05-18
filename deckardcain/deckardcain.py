5e8622a87cc99dc7a9b6284bc6398108fa58a05f
Kozejin <drunkenkoz@gmail.com>
2023-05-18 00:12:19 -0400

    [DeckardCain] Utilizing redbot.core Config

--- deckardcain/deckardcain.py
+++ deckardcain/deckardcain.py
@@ -1,37 +1,24 @@
 import discord
-from redbot.core import commands
+from redbot.core import commands, Config
 import openai
-import json
 import asyncio
 
-class DeckardCainCog(commands.Cog):
+class DeckardCain(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_keys = self.load_api_keys()
-
-    def load_api_keys(self):
-        try:
-            with open("api_keys.json", "r") as file:
-                data = json.load(file)
-                return data
-        except (FileNotFoundError, json.JSONDecodeError):
-            return {}
-
-    def save_api_keys(self):
-        with open("api_keys.json", "w") as file:
-            json.dump(self.api_keys, file)
+        self.config = Config.get_conf(self, identifier=1234567890)
+        self.config.register_guild(api_key=None)
 
     @commands.command()
     @commands.guild_only()
     @commands.is_owner()
     async def setapikey(self, ctx, api_key):
-        self.api_keys[ctx.guild.id] = api_key
-        self.save_api_keys()
+        await self.config.guild(ctx.guild).api_key.set(api_key)
         await ctx.send("API key has been set successfully.")
 
     @commands.command()
     async def askcain(self, ctx, *, question):
-        api_key = self.api_keys.get(ctx.guild.id)
+        api_key = await self.config.guild(ctx.guild).api_key()
 
         if api_key:
             response = await self.generate_response(question, api_key)
@@ -42,14 +29,11 @@ class DeckardCainCog(commands.Cog):
     async def generate_response(self, question, api_key):
         openai.api_key = api_key
 
-        chat_input = {
-            'messages': [
-                {'role': 'system', 'content': 'You are Deckard Cain, an old wise scholar.'},
-                {'role': 'user', 'content': question}
-            ]
-        }
-
-        loop = asyncio.get_event_loop()
-        response = await loop.run_in_executor(None, openai.Completion.create, 'text-davinci-003', chat_input, 0.7, 50, 1, None, 0, 0)
+        prompt = "You are Deckard Cain, an old wise scholar.\nUser: " + question
+        try:
+            response = await asyncio.to_thread(openai.Completion.create, model="text-davinci-003", prompt=prompt)
+            response_content = response["choices"][0]["text"].strip()
+        except Exception as e:
+            response_content = f"An error occurred: {str(e)}"
         
-        return response.choices[0].text.strip()
+        return response_content
