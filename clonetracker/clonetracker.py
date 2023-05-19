4d367065c7dc38b9161610227ec76aa0283c1298
Kozejin <drunkenkoz@gmail.com>
2023-05-19 10:48:46 -0400

    [CloneTacker] Update

--- clonetracker/clonetracker.py
+++ clonetracker/clonetracker.py
@@ -1,8 +1,10 @@
+import asyncio
 import discord
 from redbot.core import commands
 import aiohttp
 import json
 import datetime
+import urllib.parse
 
 class CloneTracker(commands.Cog):
     """Diablo Clone/Uber Diablo Tracker for Diablo 2: Resurrected"""
@@ -12,10 +14,26 @@ class CloneTracker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def fetch_uberd_data(self, url):
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(url) as response:
+                    if response.status != 200:
+                        return None
+                    data = await response.text()
+                    return json.loads(data)
+            except aiohttp.ClientError as e:
+                return None
+
+    async def get_uberd_url(self, region_code, ladder_code, hardcore_code):
+        base_url = "https://diablo2.io/dclone_api.php"
+        url = f"{base_url}?region={region_code}&ladder={ladder_code}&hc={hardcore_code}"
+        return url
+
     @commands.hybrid_command()
     @commands.bot_has_permissions(embed_links=True)
     async def clonetracker(self, ctx, region: str = "all", ladder: str = "all", hardcore: str = "all"):
-        """Search for diablo on all regions"""
+        """Search for Diablo on all regions"""
         valid_regions = ["americas", "europe", "asia", "all"]
         valid_ladder = ["ladder", "non-ladder", "all"]
         valid_hardcore = ["hardcore", "softcore", "all"]
@@ -50,7 +68,7 @@ class CloneTracker(commands.Cog):
         for region in regions:
             region_code = await self.get_region_code(region)
             region_name = await self.get_region_name(region_code)
-            
+
             embed = discord.Embed(title=f"Uber Diablo Information - {region_name}", description="Tracking data for Uber Diablo", color=discord.Color.blue())
             embed.set_footer(text="Data provided by Diablo2.io")
 
@@ -62,30 +80,95 @@ class CloneTracker(commands.Cog):
                     hardcore_code = await self.get_hardcore_code(hardcore_type)
                     hardcore_name = await self.get_hardcore_type(hardcore_code)
 
-                    url = f"https://diablo2.io/dclone_api.php?region={region_code}&ladder={ladder_code}&hc={hardcore_code}"
+                    url = await self.get_uberd_url(region_code, ladder_code, hardcore_code)
 
-                    async with aiohttp.ClientSession() as session:
-                        try:
-                            async with session.get(url) as response:
-                                if response.status != 200:
-                                    await ctx.send("An error occurred while fetching Uber Diablo information.")
-                                    return
+                    data = await self.fetch_uberd_data(url)
 
-                                data = await response.text()
-                                data_json = json.loads(data)
+                    if data is None:
+                        await ctx.send("An error occurred while fetching Uber Diablo information.")
+                        return
 
-                                if not data_json:
-                                    embed.add_field(name=f"{ladder_type} - {hardcore_name}", value="No information available.")
-                                else:
-                                    progress = data_json[0]['progress']
-                                    timestamp = data_json[0]['timestamped']
-                                    dt = datetime.datetime.fromtimestamp(int(timestamp))
-                                    formatted_time = dt.strftime("%I:%M %p")
-                                    embed.add_field(name=f"{ladder_type} - {hardcore_name}", value=f"Progress: {progress}/6\nLast Updated: {formatted_time}", inline=False)
-                        except aiohttp.ClientError as e:
-                            await ctx.send(f"An error occurred while fetching Uber Diablo information: {str(e)}")
+                    if not data:
+                        embed.add_field(name=f"{ladder_type} - {hardcore_name}", value="No information available.")
+                    else:
+                        progress = data[0]['progress']
+                        timestamp = data[0]['timestamped']
+                        dt = datetime.datetime.fromtimestamp(int(timestamp))
+                        formatted_time = dt.strftime("%I:%M %p")
+                        embed.add_field(name=f"{ladder_type} - {hardcore_name}", value=f"Progress: {progress}/6\nLast Updated: {formatted_time}", inline=False)
 
             await ctx.send(embed=embed)
+
+    @commands.command()
+    async def clonedatadump(self, ctx, region: str = "all", ladder: str = "all", hardcore: str = "all"):
+        """Search for Diablo on all regions (Text Format)"""
+        valid_regions = ["americas", "europe", "asia", "all"]
+        valid_ladder = ["ladder", "non-ladder", "all"]
+        valid_hardcore = ["hardcore", "softcore", "all"]
+
+        if region.lower() not in valid_regions:
+            await ctx.send("Invalid region specified. Valid regions are: Americas, Europe, Asia, All.")
+            return
+
+        if ladder.lower() not in valid_ladder:
+            await ctx.send("Invalid ladder type specified. Valid types are: Ladder, Non-Ladder, All.")
+            return
+
+        if hardcore.lower() not in valid_hardcore:
+            await ctx.send("Invalid hardcore type specified. Valid types are: Hardcore, Softcore, All.")
+            return
+
+        if region.lower() == "all":
+            regions = ["americas", "europe", "asia"]
+        else:
+            regions = [region.lower()]
+
+        if ladder.lower() == "all":
+            ladders = ["ladder", "non-ladder"]
+        else:
+            ladders = [ladder.lower()]
+
+        if hardcore.lower() == "all":
+            hardcore_types = ["hardcore", "softcore"]
+        else:
+            hardcore_types = [hardcore.lower()]
+
+        result = []
+
+        for region in regions:
+            region_code = await self.get_region_code(region)
+            region_name = await self.get_region_name(region_code)
+
+            for ladder in ladders:
+                ladder_code = await self.get_ladder_code(ladder)
+                ladder_type = await self.get_ladder_type(ladder_code)
+
+                for hardcore_type in hardcore_types:
+                    hardcore_code = await self.get_hardcore_code(hardcore_type)
+                    hardcore_name = await self.get_hardcore_type(hardcore_code)
+
+                    url = await self.get_uberd_url(region_code, ladder_code, hardcore_code)
+
+                    data = await self.fetch_uberd_data(url)
+
+                    if data is None:
+                        await ctx.send("An error occurred while fetching Uber Diablo information.")
+                        return
+
+                    if not data:
+                        text = f"No information available for {ladder_type} - {hardcore_name}"
+                    else:
+                        progress = data[0]['progress']
+                        timestamp = data[0]['timestamped']
+                        dt = datetime.datetime.fromtimestamp(int(timestamp))
+                        formatted_time = dt.strftime("%I:%M %p")
+                        text = f"[{progress}/6] - {region_name} - {ladder_type}({hardcore_name}) - [{formatted_time}]"
+
+                    result.append(text)
+
+        if result:
+            message = "\n".join(result)
+            await ctx.send(message)
 
     async def get_region_code(self, region):
         if region.lower() == "americas":
