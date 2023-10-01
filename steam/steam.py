1e7296953517aa55751910666860eebf151e808d
dkoz <drunkenkoz@gmail.com>
2023-10-01 15:42:55 -0400

    [Steam] Fixed some issues
    Some more error handling

--- steam/steam.py
+++ steam/steam.py
@@ -24,6 +24,12 @@ class SteamAPI(commands.Cog):
         url = f"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={steam_api_key}&vanityurl={custom_url}"
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
+                if response.content_type != "application/json":
+                    if isinstance(ctx, discord.Interaction):
+                        await ctx.response.send_message("The Steam API key might not be set or there's an issue with the request. Please check the API key.")
+                    else:
+                        await ctx.send("The Steam API key might not be set or there's an issue with the request. Please check the API key.")
+                    return None
                 data = await response.json()
         return data['response']['steamid'] if 'response' in data and 'steamid' in data['response'] else None
 
@@ -41,7 +47,7 @@ class SteamAPI(commands.Cog):
     @commands.is_owner()
     async def setsteamapikey(self, ctx, key: str):
         """Set the Steam API key for this guild (Server Owner Only)"""
-        await self.bot.config.guild(ctx.guild).steam_api_key.set(key)
+        await self.config.guild(ctx.guild).steam_api_key.set(key)
         await ctx.send("Steam API key has been set for this guild.")
 
     @app_commands.command(description="Search for user profiles on the Steam database.")
@@ -54,7 +60,8 @@ class SteamAPI(commands.Cog):
 
         steam_id64 = await self.get_steamid64(interaction, identifier)
         if steam_id64 is None:
-            await interaction.response.send_message("Invalid identifier. Please provide a valid SteamID64, SteamID, or custom URL.")
+            if not interaction.response.is_done():
+                await interaction.response.send_message("Invalid identifier. Please provide a valid SteamID64, SteamID, or custom URL.")
             return
 
         async with aiohttp.ClientSession() as session:
@@ -66,40 +73,32 @@ class SteamAPI(commands.Cog):
 
         if data and "response" in data and "players" in data["response"] and len(data["response"]["players"]) > 0:
             player = data["response"]["players"][0]
-
             ban_info = ban_data["players"][0] if "players" in ban_data else None
-
             steam_id = SteamID(int(steam_id64))
-
             account_creation_date = datetime.utcfromtimestamp(player['timecreated'])
             account_age = (datetime.utcnow() - account_creation_date).days // 365
-
             embed = discord.Embed(
                 title=player['personaname'],
                 url=f"https://steamcommunity.com/profiles/{steam_id64}",
                 color=discord.Color.blue()
             )
-
             embed.set_thumbnail(url=player['avatarfull'])
-
             embed.add_field(name="Profile Info", value=f"**Name:** {player.get('realname', 'Unknown')}\n**Country:** {player.get('loccountrycode', 'Unknown')}\n**Account Age:** {account_age} years", inline=True)
             embed.add_field(name="SteamID", value=f"**SteamID:** {steam_id.as_steam2}\n**SteamID3:** [U:1:{steam_id.as_32}]\n**SteamID64:** {steam_id64}", inline=True)
-
             if ban_info is not None:
                 ban_info_str = f"**VAC Banned:** {ban_info['VACBanned']}\n"
                 ban_info_str += f"**Bans:** {ban_info['NumberOfVACBans']} (Last: {ban_info['DaysSinceLastBan']} days ago)\n"
                 ban_info_str += f"**Trade Banned:** {ban_info['EconomyBan']}"
                 embed.add_field(name="Ban Info", value=ban_info_str, inline=True)
-
             embed.set_footer(text="Powered by Steam")
-
             await interaction.response.send_message(embed=embed)
         else:
-            await interaction.response.send_message("Unable to fetch the player information.")
-
+            if not interaction.response.is_done():
+                await interaction.response.send_message("Unable to fetch the player information.")
             
     @app_commands.command(description="Search for games on the Steam database.")
     async def steamgame(self, interaction: discord.Interaction, game_name: str):
+        """Search for games on the steam database."""
         url = f"https://store.steampowered.com/api/storesearch/?cc=us&l=en&term={game_name}"
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
