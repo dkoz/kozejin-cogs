4ae9ffbd3477262751478a0bfff3cb127ea99061
dkoz <drunkenkoz@gmail.com>
2024-01-18 17:29:36 -0500

    [EGNotifier] Update
    Epic changed the backend of their free game promotions.
    - Fixed slug not properly being linked.
    - Fixed promotional offers not properly displaying.
    - Added command group with administrative permissions.
    - Added new command for reposting last game offer.

--- egnotifier/egnotifier.py
+++ egnotifier/egnotifier.py
@@ -1,9 +1,10 @@
-from redbot.core import commands, Config, data_manager
 import discord
+from redbot.core import commands, Config, data_manager
 import asyncio
 import aiohttp
 import json
 import os
+import dateutil.parser
 
 class EpicGamesNotifier(commands.Cog):
     def __init__(self, bot):
@@ -35,30 +36,82 @@ class EpicGamesNotifier(commands.Cog):
                                 embed = self.create_game_embed(game)
                                 await channel.send(embed=embed)
             await asyncio.sleep(3600)
-
+            
     @commands.guild_only()
-    @commands.command()
-    async def setepicchannel(self, ctx, channel: discord.TextChannel):
+    @commands.has_permissions(administrator=True)
+    @commands.group(name="epic", aliases=["ec"], invoke_without_command=True)
+    async def epic_group(self, ctx):
+        """Commands for the Epic Games Notifier"""
+        await ctx.send_help(ctx.command)
+
+    @epic_group.command()
+    async def setchannel(self, ctx, channel: discord.TextChannel):
         """Set the channel for free game announcements."""
         await self.config.guild(ctx.guild).channel.set(channel.id)
         await ctx.send(f"Channel set to {channel.mention}")
 
-    @commands.guild_only()
-    @commands.command()
-    async def removeepicchannel(self, ctx):
+    @epic_group.command()
+    async def removechannel(self, ctx):
         """Remove the channel for free game announcements."""
         await self.config.guild(ctx.guild).channel.set(None)
         await ctx.send("Channel for free game announcements has been removed.")
+        
+    @epic_group.command()
+    async def repost(self, ctx):
+        """Repost the last game announcement."""
+        guild_id = str(ctx.guild.id)
+        if os.path.exists(self.db_path):
+            with open(self.db_path, 'r') as db_file:
+                all_posted_games = json.load(db_file)
+
+            posted_games = all_posted_games.get(guild_id, [])
+            if posted_games:
+                last_game_id = posted_games[-1]
+                url = f"https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?productId={last_game_id}"
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as response:
+                        if response.status == 200:
+                            data = await response.json()
+                            game = data['data']['Catalog']['searchStore']['elements'][0]
+                            embed = self.create_game_embed(game)
+                            await ctx.send(embed=embed)
+                        else:
+                            await ctx.send("Failed to fetch the game details.")
+            else:
+                await ctx.send("No previous game announcements found.")
+        else:
+            await ctx.send("No data file found.")
 
     def create_game_embed(self, game):
         title = game['title']
-        product_slug = game['productSlug']
-        game_url = f"https://epicgames.com/store/product/{product_slug}"
+        product_slug = game.get('productSlug')
+
+        if product_slug is None and 'catalogNs' in game and 'mappings' in game['catalogNs']:
+            for mapping in game['catalogNs']['mappings']:
+                if 'pageSlug' in mapping and 'pageType' in mapping and mapping['pageType'] == 'productHome':
+                    product_slug = mapping['pageSlug']
+                    break
+
+        game_url = f"https://store.epicgames.com/en-US/p/{product_slug}" if product_slug else "URL not available"
         description = game.get('description', 'No description available')
 
         embed = discord.Embed(title=title, url=game_url, description=description)
         embed.set_thumbnail(url=game['keyImages'][0]['url'])
-        embed.add_field(name="Available Until", value=game['expiryDate'], inline=False)
+
+        promo_offers = game.get('promotions', {}).get('promotionalOffers', [])
+        if promo_offers and promo_offers[0]['promotionalOffers']:
+            start_date = promo_offers[0]['promotionalOffers'][0].get('startDate')
+            end_date = promo_offers[0]['promotionalOffers'][0].get('endDate')
+
+            if start_date and end_date:
+                start_date = dateutil.parser.parse(start_date).strftime('%Y-%m-%d %H:%M:%S')
+                end_date = dateutil.parser.parse(end_date).strftime('%Y-%m-%d %H:%M:%S')
+                embed.add_field(name="Promotion Start", value=start_date, inline=False)
+                embed.add_field(name="Promotion End", value=end_date, inline=False)
+            else:
+                embed.add_field(name="Promotional Offer", value="No promotional dates available", inline=False)
+        else:
+            embed.add_field(name="Promotional Offer", value="No promotional offers available", inline=False)
 
         return embed
 
